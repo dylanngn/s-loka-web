a528ae308ce61d0c612e9ff2facf38d133e9db04
dylanngn <dylan.nguyen@swissmarketplace.group>
2024-06-14 11:02:40 +0700

    fix types

--- server/get-all-job.ts
+++ server/get-all-job.ts
@@ -3,11 +3,11 @@ import { getRequestContext } from '@cloudflare/next-on-pages';
 export async function getAllJob() {
   const db = getRequestContext().env.POSTS_DB;
   console.log(db)
-    const stmt = db.prepare(`select * from items where status = 3`)
+    const stmt = await db.prepare(`select * from items where status = 3`).all()
     
     if(!stmt) {
       return [] as any[]
     }
 
-    return stmt.all()
+    return stmt
 }
