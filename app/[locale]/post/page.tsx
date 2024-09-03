4df4b8be8750321c1c2547e721929e99ff4ed231
minhtu <tuminhvo98@gmail.com>
2024-09-03 21:55:19 +0700

    post

---
+++ app/[locale]/post/page.tsx
@@ -0,0 +1,23 @@
+import { Container } from "@/components/Container";
+import { Hero } from "@/components/Hero";
+import { PostTabs } from "@/components/clients/PostTabs";
+import { Locale } from "@/i18n-config";
+import { getDictionary } from "@/server/get-dictionary";
+
+export default async function AboutPage({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
+  const dict = await getDictionary(lang);
+
+  return (
+    <>
+      <Hero {...dict.Post.Hero} />
+
+      <Container className="mb-14">
+        <PostTabs leading={dict.Post.topicLeading} topics={dict.Post.topics}/>
+      </Container>
+    </>
+  );
+}
