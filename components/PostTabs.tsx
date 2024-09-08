9e607b3bf39077693add8f27de4b49a291179c31
minhtu <tuminhvo98@gmail.com>
2024-09-08 16:33:44 +0700

    misc

--- components/PostTabs.tsx
+++ components/PostTabs.tsx
@@ -46,7 +46,7 @@ export function PostTabs({ leading, topics, readTimeText }: Props) {
                 title="Bạn có đang xây dựng lối sống lành mạnh?"
                 topic="Cuộc sống"
                 readTime={`10 ${readTimeText}`}
-                path="post/ban-co-dang-xay-dung-loi-song-lanh-manh"
+                path="bai-doc/ban-co-dang-xay-dung-loi-song-lanh-manh"
               />
             ))}
           </div>
